4e8961287c741b3089011dbe159b5b9588a199a1
vasanth9 <cheepurupalli.vasanthkumar@gmail.com>
2020-09-03 11:39:05 +0530

    classes in py

---
+++ basics/classesandobjects.py
@@ -0,0 +1,48 @@
+#classes and objects
+""" 
+Python is an object oriented programming language.
+
+Almost everything in Python is an object, with its properties and methods.
+
+A Class is like an object constructor, or a "blueprint" for creating objects.
+""" 
+class myclass:
+    x=5
+p1=myclass()
+print(p1.x)
+"""
+All classes have a function called __init__(), which is always executed when the class is being initiated.
+
+Use the __init__() function to assign values to object properties, or other operations that are necessary to do when the object is being created:
+"""
+class Person:
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+    def printname(self):
+        print("my name is",self.name)
+p1=Person("vasanth",19)
+print(p1.name,"  ",p1.age)
+p1.printname()
+#change values
+p1.age=40
+print(p1.age)
+#del
+del p1.age
+print(p1)
+print(p1.name)
+""" print(p1.age) gives error age was deleted
+Traceback (most recent call last):
+  File "classesandobjects.py", line 32, in <module>
+    print(p1.age)
+AttributeError: 'Person' object has no attribute 'age'
+"""
+del p1 #object deletion
+"""
+5
+vasanth    19
+my name is vasanth
+40
+<__main__.Person object at 0x7f3d8d87d8e0>
+vasanth
+"""
